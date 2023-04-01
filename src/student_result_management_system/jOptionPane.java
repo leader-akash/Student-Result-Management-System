00629c00537613d89c409bb86ea18002f0429be0
leader-akash <117633377+leader-akash@users.noreply.github.com>
2023-04-01 09:42:55 +0530

    Add files via upload

---
+++ src/student_result_management_system/jOptionPane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student_result_management_system;
+
+/**
+ *
+ * @author Akash
+ */
+class jOptionPane {
+
+    static void showMessageDialog(Object object, String toString) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
